dc784fedacc2daba6f934b80ba9efe7f8e4d712b
mori1365 <105076694+mori1365@users.noreply.github.com>
2024-03-11 21:52:18 +0330

    update2

--- main.js
+++ main.js
@@ -8,3 +8,13 @@ menu.addEventListener("click", function() {
         nav.style.left="100%";
     }
 })
+let fullbox = document.querySelector(".fullscreen");
+        let closes = document.querySelector(".closed");
+        let iframe = document.querySelector("iframe");
+        function openbox(url) {
+            fullbox.style.left = 0;
+            iframe.src = url;
+        }
+        closes.onclick = () => {
+            fullbox.style.left = "100%";
+        }
